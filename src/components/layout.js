24ff227f8d36da9b5a79354036540c0769730790
Igor Slabykh <igor.slabykh@gmail.com>
2020-03-12 22:38:31 -0400

    made nav bar in layout responsive. still working on mobile version

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,10 +27,10 @@ const LinkH4 = styled.h4`
   line-height: 20px;
   letter-spacing: 0.1em;
   text-decoration: none;
+  box-shadow: none;
   @media (max-width: 600px) {
     border: 1px solid black;
     margin: 5px;
-    text-decoration: none;
   }
 `
 
